d45fc39bf3554371df274b42f8889daf09ec0b9f
Humna-Muzammil <humnamuzammil0@gmail.com>
2025-06-15 19:12:05 +0500

    Add files via upload

--- profinal.c
+++ profinal.c
@@ -1,46 +1,48 @@
 #include <stdio.h>
 #include <math.h>
 
-#define PI  3.14159265358979323846
+#define PI  3.14159
 
 int main() {
-    char operation;
-    double num1, num2, result = 0;
+char operation;
+double num1, num2, result = 0;
 printf("________________________________________\n");
 printf("\nWELCOME TO HUMNA's Scientific Calculator\n");
  printf("________________________________________\n");
 printf("Enter ]ur operation (+, -, *, /, sin, cos, tan, log, sqrt, exp): ");
-    scanf(" %c", &operation);
-    if (operation == '+' || operation == '-' || operation == '*' || operation == '/') {
-        printf("Enter two numbers: ");
-        scanf("%lf %lf", &num1, &num2);
+scanf(" %c", &operation);
+if (operation == '+' || operation == '-' || operation == '*' || operation == '/') {
+printf("Enter two numbers: ");
+scanf("%lf %lf", &num1, &num2);
         
 switch (operation) {
-case '+': result = num1 + num2; break;
-case '-': result = num1 - num2; break;
- case '*': result = num1 * num2; break;
+case '+': result = num1 + num2; 
+break;
+case '-': result = num1 - num2; 
+break;
+ case '*': result = num1 * num2; 
+ break;
 case '/': 
 if (num2 != 0)
 result = num1 / num2;
-                else {
-                    printf("Enter valid input!!.\n");
-                    return 1;
-                }
-                
-                break;
+else {
+printf("Enter valid input!!.\n");
+return 1;
+ }
+ break;
         }
-        printf("Result: %.6lf\n", result);
+printf("Result: %lf\n", result);
     }
     else {
-        printf("Enter a  degree number : ");
+printf("Enter a   number for degree : ");
         scanf("%lf", &num1);
 
 if          (operation == 's') result = sin(num1 * PI / 180);  
 else if  (operation == 'c') result = cos(num1 * PI / 180);
-else if   (operation == 't') result = tan(num1 * PI / 180);
-else if   (operation == 'l') result = log(num1);
-else if   (operation == 'q') result = sqrt(num1*num1);
-else if (operation == 'e') result = exp(num1);
+else if   (operation =='t') result = tan(num1 * PI / 180);
+else if   (operation =='l') result = log(num1);
+else if   (operation =='q') result = sqrt(num1*num1);
+else if (operation =='e') result = exp(num1);
         else {
                
             printf("Invalid operation Try again!!\n");
